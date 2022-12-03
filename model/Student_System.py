c8e7b24e63db8414c45628f537d410f1ae439ed9
ConstanceLxy <109813903+ConstanceLxy@users.noreply.github.com>
2022-12-02 21:26:46 -0800

    Update Student_System.py

---
+++ model/Student_System.py
@@ -0,0 +1,38 @@
+from Student_log_in import log_in
+from Student_load import load
+from Student_search import part_search
+from Student_operate import part_operate
+from Student_save import part_save
+from Student_record import record
+log_in() #log in
+students = load()      #load
+f = open('record.txt','w')
+f.close()
+while True:
+    print("""
+                Student score management System 
+                Search.............1
+                operate.............2
+                save.............3
+                exit.................4
+    """)
+    choose = 0 
+    try:
+        choose = int(input('Please input num 1-4：'))
+    except:
+        print("")
+    if choose == 1:
+        record('system',choose)
+        part_search(students)
+    elif choose == 2:
+        record('system',choose)
+        part_operate(students)
+    elif choose == 3:
+        record('system',choose)
+        part_save(students)
+    elif choose == 4:
+        record('system',choose)
+        print('close')
+        exit(0)
+    else:
+        print('Invalid input,please input again!')
