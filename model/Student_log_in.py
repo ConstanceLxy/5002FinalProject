44a0d75204d82d874b42c2b6ca3dcd0f6c76a193
JiaHao Wang <wangjiahao96923@gmail.com>
2022-12-06 18:25:01 -0800

    Final Version V4.0 login update

--- model/Student_log_in.py
+++ model/Student_log_in.py
@@ -1,27 +1,31 @@
 import time
-answerName = 0
-answerPassword = 0
+enterName = 0
+enterPassword = 0
 name1 = 'Teacher'
 password1 = '5002111'
 Time = 0
 Try = 4
 for i in range(5):
+    # use for loop to enable people to enter username and password many times.
     if Try !=0:
-        answerName = input("Username: ")
-        if answerName == 'Teacher':
-            answerPassword = input("Password: ")
-            if answerPassword == '5002111':
+        enterName = input("Username: ")
+        if enterName == "Teacher":
+            enterPassword = input("Password: ")
+            if enterPassword == "5002111":
                 for i in range(10):
-                    time.sleep(0.1)
+                    time.sleep(0.05)
+                    # We use time.sleep() to defer execution, in this way, we can imitate an old computer to login.
                     Time+=10
                     print("login...(",Time,"/100)")
                 print("log in successfully")
                 break
             else:
-                print('Incorrect password. You can try', Try, 'more times.')
+                print('Incorrect password. You can try', Try-1, 'more times.')
+                # People can only try 4 times to enter password.
                 Try-=1
         else:
-            print('Incorrect username. You can try',Try, 'more times.')
+            print('Incorrect username. You can try',Try-1, 'more times.')
+            # People can only try 4 times to enter username.
             Try-=1
     else:
         print('No more chance.')
